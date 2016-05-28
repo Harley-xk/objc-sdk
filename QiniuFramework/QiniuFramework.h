bce383aa2cd99c920fb9ea975e570598eee1cbc4
Harley.xk <harley.gb@foxmail.com>
2016-05-28 17:14:17 +0800

    修复 Carthage 支持问题

---
+++ QiniuFramework/QiniuFramework.h
@@ -0,0 +1,25 @@
+//
+//  QiniuFramework.h
+//  QiniuFramework
+//
+//  Created by Harley.xk on 16/5/28.
+//  Copyright © 2016年 Qiniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QiniuFramework.
+FOUNDATION_EXPORT double QiniuFrameworkVersionNumber;
+
+//! Project version string for QiniuFramework.
+FOUNDATION_EXPORT const unsigned char QiniuFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QiniuFramework/PublicHeader.h>
+
+#import "QNConfiguration.h"
+#import "QNFileRecorder.h"
+#import "QNResponseInfo.h"
+#import "QNUploadManager.h"
+#import "QNUploadOption.h"
+#import "QNUrlSafeBase64.h"
+
